bb3612616bb5cf090fd13a4338fb664a644cacad
Tokusei Noborio <tokusei@nyampass.com>
2022-01-01 00:03:02 +0900

    add next-board-test

--- src/disposable_chopsticks/board.clj
+++ src/disposable_chopsticks/board.clj
@@ -5,7 +5,8 @@
 
 (defprotocol IBoard
   (state [this])
-  (actions [this turn]))
+  (actions [this turn])
+  (next-board [this action]))
 
 (defn- -hands [board turn]
   (let [{first-hands :1st second-hands :2nd} (state board)]
@@ -22,25 +23,42 @@
          :when (not= [left right] [(- right i) (+ left i)])]
      [1 0 i])))
 
-(defn- -opposite-actions [[left right] opposite-hand opposite-index]
+(defn- -opposite-actions [[left right] inc-index opposite-hand opposite-index]
   (filter identity
           [(when (and (> left 0)
                       (> opposite-hand 0))
-             [0 opposite-index])
+             [(+ 0 inc-index) opposite-index])
            (when (and (> right 0)
                       (> opposite-hand 0))
-             [1 opposite-index])]))
+             [(+ 1 inc-index) opposite-index])]))
 
-(defn- -actions [[my-hands [opposite-left opposite-right]]]
-  (concat
-   (-my-actions my-hands)
-   (-opposite-actions my-hands opposite-left 2)
-   (-opposite-actions my-hands opposite-right 3)))
+(defn- -actions [[my-hands [opposite-left opposite-right]] turn]
+  (let [inc-index (if (= turn :1st) 0 2)]
+    (concat
+     (map (fn [[from to amount]]
+            (remove nil? [(+ from inc-index) (+ to inc-index) amount]))
+          (-my-actions my-hands))
+     (-opposite-actions my-hands inc-index opposite-left (mod (+ inc-index 2) 4))
+     (-opposite-actions my-hands inc-index opposite-right (mod (+ inc-index 3) 4)))))
+
+(defn- -next-board [state [from to amount]]
+  (let [{first-hands :1st second-hands :2nd} state
+        seq-hands (vec (concat first-hands second-hands))
+        [first-hands second-hands]
+        (->> (if amount
+               (-> seq-hands
+                   (assoc from (- (nth seq-hands from) amount))
+                   (assoc to (+ (nth seq-hands to) amount)))
+               (assoc seq-hands to (+ (nth seq-hands to) (nth seq-hands from))))
+             (map #(mod %1 5))
+             (partition 2))]
+    {:1st first-hands :2nd second-hands}))
 
 (deftype Board [state]
   IBoard
   (state [_] state)
-  (actions [this turn] (-actions (-hands this turn))))
+  (actions [this turn] (-actions (-hands this turn) turn))
+  (next-board [_ action] (Board. (-next-board state action))))
 
 (defn board
   [& {first-hands :1st second-hands :2nd}]
--- test/disposable_chopsticks/board_test.clj
+++ test/disposable_chopsticks/board_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer [deftest is]]
             [disposable-chopsticks.board :refer :all]))
 
+(actions (board) :1st)
+
 (deftest initial-board-test
   (is (= (state (board))
          {:1st [1 1] :2nd [1 1]}))
@@ -18,15 +20,31 @@
     (is (some #{[1 2]} first-actions))
     (is (some #{[1 3]} first-actions))))
 
+(deftest second-action-test
+  (let [second-actions (actions (board :1st [1 2] :2nd [3 0]) :2nd)]
+    (is (= (count second-actions) 4))
+    (is (some #{[2 3 1]} second-actions))
+    (is (some #{[2 3 2]} second-actions))
+    (is (some #{[2 0]} second-actions))
+    (is (some #{[2 1]} second-actions))))
+
+
 (deftest loop-my-action-test
   (is (not (some #{[0 1 1]} (actions (board :1st [2 1] :2nd [0 1]) :1st))))
   (is (not (some #{[1 0 2]} (actions (board :1st [1 3] :2nd [0 1]) :1st)))))
 
 (deftest opponent-cant-be-place-action-test
   (is (not (some #{2}
-                 (map first (actions
-                             (board :1st [2 1] :2nd [0 0]) :1st)))))
+                 (map first (actions (board :1st [2 1] :2nd [0 0]) :1st)))))
   (is (not (some #{3}
-                 (map first (actions
-                             (board :1st [1 4] :2nd [0 0]) :1st))))))
+                 (map first (actions (board :1st [1 4] :2nd [0 0]) :1st))))))
 
+(deftest next-board-test
+  (is (= (state (next-board (board) [0 1 1]))
+         {:1st [0 2] :2nd [1 1]}))
+  (is (= (state (next-board (board) [1 3]))
+         {:1st [1 1] :2nd [1 2]}))
+  (is (= (state (next-board (board :1st [4 0] :2nd [1 2]) [2 0]))
+         {:1st [0 0] :2nd [1 2]}))
+  (is (= (state (next-board (board :1st [4 0] :2nd [1 2]) [3 0]))
+         {:1st [1 0] :2nd [1 2]})))
