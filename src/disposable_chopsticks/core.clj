2daa84501019d530382cab41553ec6fbba15676d
Tokusei Noborio <tokusei@nyampass.com>
2022-01-01 10:16:24 +0900

    add first-uniq-action-test

--- src/disposable_chopsticks/core.clj
+++ src/disposable_chopsticks/core.clj
@@ -1,16 +1,40 @@
 (ns disposable-chopsticks.core
-  (:use [clojure.set :only [map-invert]]
-        [clojure.string :only [split]])
+  (:require [disposable-chopsticks.board :refer :all])
   (:gen-class))
 
+(def next-player {:1st :2nd, :2nd :1st})
+
+(def letter-vec (seq (char-array "abcd")))
+
+(def whose-turn {0 :1st, 1 :2nd})
+
+(defn letter-index [v]
+  (let [index (.indexOf letter-vec v)]
+    (when (>= index 0) index)))
+
+(def computer :2nd)
+
+(defn print-board [board]
+  (let [{first :1st second :2nd} (state board)]
+    (dotimes [n 2]
+      (println "    "
+               (apply str (interpose " " (nth (partition 2 letter-vec) n))))
+      (println (name (whose-turn n))
+               (if (= n 0) first second)))))
+
+(defn parse-action [str]
+  (map {\a 0 \b 1 \c 2 \d 3
+        \1 1 \2 2 \3 3 \4 4} str))
+
+(parse-action "ab3")
+
 (defn get-winner-message
-  "Returns a string message of the winner"
   [winner]
-  (str "Player " (board-symbol-map winner) " has wone. Congrats!"))
+  (str "Player " (name winner) " has wone. Congrats!"))
 
 (defn get-curr-player-message
   [curr-player]
-  (let [base (str "Current player: " (board-symbol-map curr-player))]
+  (let [base (str "Current player: " (name curr-player))]
     (if (= curr-player computer)
       (str base " (the computer)")
       base)))
@@ -21,47 +45,19 @@
   [n]
   (whose-turn (mod (- n 1) 2)))
 
-(defn get-other-player
-  [curr-player]
-  (cond
-    (= curr-player :x) :o
-    (= curr-player :o) :x))
-
-;;
-;; Min-max and related functions
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn utility
-  "Utility function for the min-max algorithm.
-  :x is the max player and :o is the min player" ; Computer is :o
   [board]
-  (cond
-    (has-player-won board :x) 1
-    (has-player-won board :o) -1
-    :else nil))
-
-(defn get-nil-indices
-  "For a particular row, returns the indices of its nil items."
-  [row]
-  (keep-indexed #(if (nil? %2) %1) row))
+  (prn :utility board (state board))
+  (condp = (won? board)
+    :1st 1
+    :2nd -1
+    nil))
 
 (defn attach-row-coord
   "Given a row-coord (e.g. 2) and an items list e.g. (0 1), returns a list of lists where
   row-coord is attached to each item e.g. ( (2 0) (2 1) )"
   [row-coord items-list]
   (map #(list row-coord %) items-list))
-
-(defn get-empty-squares
-  "returns a list of (row, col) lists that are the coords of the nil squares in board"
-  [board]
-  (apply concat (keep-indexed #(attach-row-coord %1 %2)
-                              (map get-nil-indices board))))
-
-(defn get-all-actions
-  "Returns a list of all possible actions (boards) for the curr-player from board"
-  [board curr-player]
-  (map (fn [x] (update-board board (first x) (last x) curr-player)) (get-empty-squares board)))
-
 
 (defn get-comparison-fn
   "Returns the appropriate comparison function for minmax"
@@ -70,60 +66,51 @@
     (= curr-player :x) >
     (= curr-player :o) <))
 
-(defn minmax
-  "Recursive minmax implementation.
-  n is the relative depth of board, i.e. 1 = 1 ply search, 2 = 2 ply search, etc..."
-  [curr-board curr-player counter]
-  (let [cur-util (utility curr-board)]
-    (if (not (nil? cur-util))            ; stopping criteria in recursive call
+(defn minmax [board player counter]
+  (prn :minmax board player counter)
+  (let [cur-util (utility board)]
+    (if (not (nil? cur-util))
       cur-util
-      (do
-        (let [actions (get-all-actions curr-board curr-player)
-              comp-fn (get-comparison-fn curr-player)]
-          (let [action-util-pairs (map (fn [a] (list a (minmax a (get-other-player curr-player) (inc counter)))) actions) ; recursively create tree
-                best-one (first (sort-by last comp-fn action-util-pairs))]        ; percolate best-move up the tree
-;(println (str "DEBUG: best one is: " best-one " counter is: " counter))
-            (if (== counter 1)
-              (first best-one)            ; choose best move by board
-              (last best-one))))))))      ; choose best move by utility function
+      (let [actions (actions board player)
+            comp-fn (get-comparison-fn player)]
+        (println :actions actions)
+        (let [action-util-pairs
+              (map (fn [action] (list action (minmax (next-board board action) (next-player player) (inc counter)))) actions) ; recursively create tree
+              best-one (first (sort-by last comp-fn action-util-pairs))]        ; percolate best-move up the tree
+          (println :action-util-pairs action-util-pairs )
+          (println :best-one best-one )
+          (println (str "DEBUG: best one is: " best-one " counter is: " counter))
+          (if (= counter 1)
+            (first best-one)            ; choose best move by board
+            (last best-one)))))))      ; choose best move by utility function
 
-(defn apply-move
-  [board curr-player]
-  (if (= curr-player computer)
+(defn apply-move-board
+  [board player]
+  (prn :apply-move-board board player)
+  (if (= player computer)
     (do
-      (minmax board curr-player 1))
+      (minmax board player 1))
     (do
       (println "Your move: (<from letter><to letter><number>)")
-      (let [action (get-action (read-line))]
+      (let [action (parse-action (read-line))]
         (println)
         (if (nil? action)
           board
-          (update-board board action curr-player))))))
+          (next-board board action))))))
 
-;;
-;; Main routine
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn -main
   [& args]
-  (println  "Starting waribashi game.")
-  (loop [n 0
-         board (get-empty-board)]
-    (let [curr-player (whose-turn (mod n 2))
-          prev-player (get-prev-player n)]
-      (println (get-curr-player-message curr-player))
-      (print-board board)
-      (cond
-        (has-player-won board prev-player)
-        (println (get-winner-message prev-player))
-        :else
-        (do
-          (let [new-board (apply-move board curr-player)]
-            (cond
-              (= new-board board)
-              (do
-                (println "Invalid move. Try again\n")
-             ; There was an invalid move, so keep the same player
-                (recur n board))
-              :else ; valid move, so move on to next player
-              (recur (+ n 1) new-board))))))))
+  (loop [player :1st
+         board (new-board)]
+    (println (get-curr-player-message player))
+    (print-board board)
+    (prn "hoge")
+    (if-let [won-player (won? board)]
+      (println (get-winner-message won-player))
+      (let [applied-board (apply-move-board board player)]
+        (prn :applied-board applied-board :board board)
+        (if (= (state applied-board) (state board))
+          (do
+            (println "Invalid move. Try again\n")
+            (recur player board))
+          (recur (next-player player) applied-board))))))
--- test/disposable_chopsticks/board_test.clj
+++ test/disposable_chopsticks/board_test.clj
@@ -3,13 +3,13 @@
             [disposable-chopsticks.board :refer :all]))
 
 (deftest initial-board-test
-  (is (= (state (board))
+  (is (= (state (new-board))
          {:1st [1 1] :2nd [1 1]}))
-  (is (= (state (board :1st [1 2] :2nd [3 4]))
+  (is (= (state (new-board :1st [1 2] :2nd [3 4]))
          {:1st [1 2] :2nd [3 4]})))
 
 (deftest first-action-test
-  (let [first-actions (actions (board) :1st)]
+  (let [first-actions (actions (new-board) :1st)]
     (is (= (count first-actions) 6))
     (is (some #{[0 1 1]} first-actions))
     (is (some #{[1 0 1]} first-actions))
@@ -19,7 +19,7 @@
     (is (some #{[1 3]} first-actions))))
 
 (deftest second-action-test
-  (let [second-actions (actions (board :1st [1 2] :2nd [3 0]) :2nd)]
+  (let [second-actions (actions (new-board :1st [1 2] :2nd [3 0]) :2nd)]
     (is (= (count second-actions) 4))
     (is (some #{[2 3 1]} second-actions))
     (is (some #{[2 3 2]} second-actions))
@@ -27,26 +27,32 @@
     (is (some #{[2 1]} second-actions))))
 
 (deftest loop-my-action-test
-  (is (not (some #{[0 1 1]} (actions (board :1st [2 1] :2nd [0 1]) :1st))))
-  (is (not (some #{[1 0 2]} (actions (board :1st [1 3] :2nd [0 1]) :1st)))))
+  (is (not (some #{[0 1 1]} (actions (new-board :1st [2 1] :2nd [0 1]) :1st))))
+  (is (not (some #{[1 0 2]} (actions (new-board :1st [1 3] :2nd [0 1]) :1st)))))
 
 (deftest opponent-cant-be-place-action-test
   (is (not (some #{2}
-                 (map first (actions (board :1st [2 1] :2nd [0 0]) :1st)))))
+                 (map first (actions (new-board :1st [2 1] :2nd [0 0]) :1st)))))
   (is (not (some #{3}
-                 (map first (actions (board :1st [1 4] :2nd [0 0]) :1st))))))
+                 (map first (actions (new-board :1st [1 4] :2nd [0 0]) :1st))))))
+
+(deftest first-uniq-action-test
+  (let [first-actions (uniq-actions (new-board) :1st)]
+    (is (= (count first-actions) 2))
+    (is (some #{[0 1 1]} first-actions))
+    (is (some #{[0 2]} first-actions))))
 
 (deftest next-board-test
-  (is (= (state (next-board (board) [0 1 1]))
+  (is (= (state (next-board (new-board) [0 1 1]))
          {:1st [0 2] :2nd [1 1]}))
-  (is (= (state (next-board (board) [1 3]))
+  (is (= (state (next-board (new-board) [1 3]))
          {:1st [1 1] :2nd [1 2]}))
-  (is (= (state (next-board (board :1st [4 0] :2nd [1 2]) [2 0]))
+  (is (= (state (next-board (new-board :1st [4 0] :2nd [1 2]) [2 0]))
          {:1st [0 0] :2nd [1 2]}))
-  (is (= (state (next-board (board :1st [4 0] :2nd [1 2]) [3 0]))
+  (is (= (state (next-board (new-board :1st [4 0] :2nd [1 2]) [3 0]))
          {:1st [1 0] :2nd [1 2]})))
 
 (deftest won?-test
-  (is (= (won? (board)) nil))
-  (is (= (won? (board :1st [0 0] :2nd [2 3])) :1st))
-  (is (= (won? (board :1st [2 3] :2nd [0 0])) :2nd)))
+  (is (= (won? (new-board)) nil))
+  (is (= (won? (new-board :1st [0 0] :2nd [2 3])) :1st))
+  (is (= (won? (new-board :1st [2 3] :2nd [0 0])) :2nd)))
